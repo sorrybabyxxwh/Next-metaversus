faf1cac8d214bd140c184f416af65af6cee6d14e
Alisa Velyka <sorrybabyxxwh@gmail.com>
2023-01-17 23:14:46 +0200

    first commit

---
+++ .next/static/X7HPKTutEUwxVdugBD92j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/910-b79b5d8c24e05cc6.js","static/chunks/pages/index-f48bc9ce69033ca6.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
